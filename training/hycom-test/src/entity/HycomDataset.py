d5df9f7c53626c0da8f2691aa184fa5794360747
Darren Adams <dadams@illinois.edu>
2021-08-29 08:22:22 -0500

    added createHycomDatasetFromCatalog method to HycomDataset

---
+++ training/hycom-test/src/entity/HycomDataset.py
@@ -0,0 +1,39 @@
+import requests
+import xmltodict
+#from urllib.parse import urlencode,urljoin
+
+def createHycomDatasetFromCatalog(url=None):
+    """
+    Query Hycom catalog and create HycomDataset object.
+    """
+    with requests.get(url) as r:
+        doc = xmltodict.parse(r.text)
+    name = doc['catalog']['@name']
+    version = doc['catalog']['@version']
+    id = name.rpartition('/')[2]+'_'+version
+    lat_start = float(doc['catalog']['dataset']['metadata']['geospatialCoverage']['northsouth']['start'])
+    lat_size = float(doc['catalog']['dataset']['metadata']['geospatialCoverage']['northsouth']['size'])
+    lon_start = float(doc['catalog']['dataset']['metadata']['geospatialCoverage']['eastwest']['start'])
+    lon_size = float(doc['catalog']['dataset']['metadata']['geospatialCoverage']['eastwest']['size'])
+    
+    dataset_spec = {
+        'id': id,
+        'name': id,
+        'description': name,
+        'hycom_version': version,
+        'geospatialCoverage': {
+            'start': {
+                'latitude': lat_start,
+                'longitude': lon_start
+            },
+            'end': {
+                'latitude': lat_start + lat_size,
+                'longitude': lon_start + lon_size
+            }
+        }
+        
+    }
+    dataset = c3.HycomDataset(**dataset_spec)
+    dataset.upsert()
+    #print(dataset_spec)
+    return dataset
