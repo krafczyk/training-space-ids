e2b90722db409b40ede5aeca4a244896c4b256d6
Darren Adams <dadams@illinois.edu>
2021-08-30 10:07:07 -0500

    tweaked some fields

--- training/hycom-test/src/entity/HycomDataset.py
+++ training/hycom-test/src/entity/HycomDataset.py
@@ -1,6 +1,6 @@
 import requests
 import xmltodict
-from urllib.parse import urlencode,urljoin
+
 
 def upsertHycomDatasetFromCatalog(url=None):
     """
@@ -41,37 +41,6 @@ def upsertHycomDatasetFromCatalog(url=None):
     #print(dataset_spec)
     return dataset
     
-def buildHycomFMRCUrl(urlpath,time_start,time_end,
-                      vars=['surf_el','salinity','water_temp','water_u','water_v'],
-                      disableLLSubset='on',
-                      disableProjSubset='on',
-                      horizStride=1,
-                      timeStride=1,
-                      vertStride=1,
-                      addLatLon='true',
-                      accept='netcdf4'
-                     ):
-    base_url=f"https://ncss.hycom.org/thredds/ncss/{urlpath}"
-    #print(base_url)
-    varst = [('var',v) for v in vars]
-    url1 = urlencode(varst,{'d':2})
-    url3 = urlencode({'disableLLSubset':disableLLSubset,
-                      'disableProjSubset':disableProjSubset,
-                      'horizStride':horizStride,
-                      'timeStride':timeStride,
-                      'vertStride':vertStride,
-                      'addLatLon':addLatLon,
-                      'accept':accept
-                     })
-    if (time_start == time_end):
-        url2 = urlencode({'time':time_start})
-    else:
-        url2 = urlencode({'time_start':time_start,'time_end':time_end})
-
-    query = url1 + '&' + url2 + '&' + url3
-    url = base_url+'?'+query
-    return url  
-
 
 
 def upsertFMRCFromDatasetCatalog(this):
@@ -85,15 +54,11 @@ def upsertFMRCFromDatasetCatalog(this):
             'id': d['@ID'],
             'dataset': this,
             'run': d['@name'],
+            'urlPath': d['@urlPath']
             'timeCoverage': {
                 'start':d['timeCoverage']['start'],
                 'end':d['timeCoverage']['end'],
             },
-            # 'thredds_url': buildHycomFMRCUrl(
-            #        urlpath = d['@urlPath'],
-            #        time_start = d['timeCoverage']['start'],
-            #        time_end = d['timeCoverage']['end']
-            #        )
         }
     ).upsert() for d in doc['catalog']['dataset']['dataset']
             ]
