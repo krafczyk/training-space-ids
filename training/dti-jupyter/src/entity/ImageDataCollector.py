416db25e8943d6f94f0063c77ede866a06d12e52
Yifang Zhang <zhang303@illinois.edu>
2021-09-02 18:19:42 -0500

    adding method get_metadata_from_url

--- training/dti-jupyter/src/entity/ImageDataCollector.py
+++ training/dti-jupyter/src/entity/ImageDataCollector.py
@@ -1,12 +1,17 @@
 import io
 import numpy as np
 import urllib
-import os
+from PIL import Image
 
-def get_image_from_url(input_url):
+
+def get_metadata_from_url(input_url):
+    
+    pil_im = None
     ## start request the image ##
-    with urllib.request.urlopen(input_url) as url:
-        with open('temp_cluster.jpg', 'wb') as f:
-            f.write(url.read)
-    ## return the pil_im ##
-    return os.getcwd() + 'temp_cluster.jpg'
+    with urllib.request.urlopen(cat1_IDC.blob_url) as url:
+        temp_img = url.read()
+        pil_im = Image.open(io.BytesIO(temp_img), 'r')
+    ## assign the image info into the C3 Type ##
+    self.image_width, self.image_height = pil_im.size
+
+    return -1
