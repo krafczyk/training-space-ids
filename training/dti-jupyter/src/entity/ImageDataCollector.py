ffa29a959043abc4778e0d78da9a998a5aa149d1
Yifang Zhang <zhang303@illinois.edu>
2021-09-02 20:25:48 -0500

    first try for member function

--- training/dti-jupyter/src/entity/ImageDataCollector.py
+++ training/dti-jupyter/src/entity/ImageDataCollector.py
@@ -4,7 +4,7 @@ import urllib
 from PIL import Image
 
 
-def get_metadata_from_url(input_url):
+def get_metadata_from_url(this, input_url):
     
     pil_im = None
     ## start request the image ##
@@ -12,6 +12,6 @@ def get_metadata_from_url(input_url):
         temp_img = url.read()
         pil_im = Image.open(io.BytesIO(temp_img), 'r')
     ## assign the image info into the C3 Type ##
-    self.image_width, self.image_height = pil_im.size
+    this.image_width, this.image_height = pil_im.size
 
     return -1
