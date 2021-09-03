859767b1c0c6f9ae2549eb00153e7dc67275bd90
Yifang Zhang <zhang303@illinois.edu>
2021-09-02 20:29:35 -0500

    2nd try on the member function

--- training/dti-jupyter/src/entity/ImageDataCollector.py
+++ training/dti-jupyter/src/entity/ImageDataCollector.py
@@ -8,7 +8,7 @@ def get_metadata_from_url(this, input_url):
     
     pil_im = None
     ## start request the image ##
-    with urllib.request.urlopen(cat1_IDC.blob_url) as url:
+    with urllib.request.urlopen(input_url) as url:
         temp_img = url.read()
         pil_im = Image.open(io.BytesIO(temp_img), 'r')
     ## assign the image info into the C3 Type ##
