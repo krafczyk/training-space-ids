f87eeda747ba260264db89d8473a8c3bb9599a93
Yifang Zhang <zhang303@illinois.edu>
2021-09-02 21:01:45 -0500

    see if other parts are functional or not

--- training/dti-jupyter/src/entity/ImageDataCollector.py
+++ training/dti-jupyter/src/entity/ImageDataCollector.py
@@ -6,12 +6,12 @@ def get_metadata_from_url(this, input_url):
     import urllib
     from PIL import Image
 
-    ##pil_im = None
+    pil_im = None
     ## start request the image ##
-    ##with urllib.request.urlopen(input_url) as url:
-        ##temp_img = url.read()
-        ##pil_im = Image.open(io.BytesIO(temp_img), 'r')
+    with urllib.request.urlopen(input_url) as url:
+        temp_img = url.read()
+        pil_im = Image.open(io.BytesIO(temp_img), 'r')
     ## assign the image info into the C3 Type ##
-    this.image_width, this.image_height = 200,180 ##pil_im.size
+    #this.image_width, this.image_height = 200,180 ##pil_im.size
 
     return -1
