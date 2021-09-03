7e0a78d100a252561ff591fc9c633eca13b06b20
Yifang Zhang <zhang303@illinois.edu>
2021-09-02 20:53:46 -0500

    testing if member function is working

--- training/dti-jupyter/src/entity/ImageDataCollector.py
+++ training/dti-jupyter/src/entity/ImageDataCollector.py
@@ -1,17 +1,17 @@
-import io
-import numpy as np
-import urllib
-from PIL import Image
-
-
+## funcions ##
 def get_metadata_from_url(this, input_url):
     
-    pil_im = None
+    import io
+    import numpy as np
+    import urllib
+    from PIL import Image
+
+    ##pil_im = None
     ## start request the image ##
-    with urllib.request.urlopen(input_url) as url:
-        temp_img = url.read()
-        pil_im = Image.open(io.BytesIO(temp_img), 'r')
+    ##with urllib.request.urlopen(input_url) as url:
+        ##temp_img = url.read()
+        ##pil_im = Image.open(io.BytesIO(temp_img), 'r')
     ## assign the image info into the C3 Type ##
-    this.image_width, this.image_height = pil_im.size
+    this.image_width, this.image_height = 200,180 ##pil_im.size
 
     return -1
