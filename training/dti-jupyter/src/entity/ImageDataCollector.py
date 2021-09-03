4690cbd242cdd592b983256f06b63b1e1ca49a8d
Yifang Zhang <zhang303@illinois.edu>
2021-09-02 21:08:54 -0500

    change to hello world

--- training/dti-jupyter/src/entity/ImageDataCollector.py
+++ training/dti-jupyter/src/entity/ImageDataCollector.py
@@ -1,17 +1,19 @@
 ## funcions ##
 def get_metadata_from_url(this, input_url):
     
-    import io
-    import numpy as np
-    import urllib
-    from PIL import Image
+    ##import io
+    ##import numpy as np
+    ##import urllib
+    ##from PIL import Image
 
-    pil_im = None
+    ##pil_im = None
     ## start request the image ##
-    with urllib.request.urlopen(input_url) as url:
-        temp_img = url.read()
-        pil_im = Image.open(io.BytesIO(temp_img), 'r')
+    ##with urllib.request.urlopen(input_url) as url:
+        ##temp_img = url.read()
+        ##pil_im = Image.open(io.BytesIO(temp_img), 'r')
     ## assign the image info into the C3 Type ##
     #this.image_width, this.image_height = 200,180 ##pil_im.size
+
+    print("hello world!")
 
     return -1
