759b5495ed218f1e02d9a938b0450edd20a8fade
Darren Adams <dadams@illinois.edu>
2021-09-06 19:44:29 -0500

    syntax

--- training/hycom-test/src/entity/FMRCFile.py
+++ training/hycom-test/src/entity/FMRCFile.py
@@ -45,7 +45,7 @@ def download(this):
         options['time_end'] = time_end
 
     vars_list = this.vars.split(',')
-    vars = [('var',v) for v in vars_list+]
+    vars = [('var',v) for v in vars_list]
     url1 = urlencode(vars,{'d':2})
     url2 = urlencode(options)
     url = urljoin(baseurl,url1+'&'+url2)
