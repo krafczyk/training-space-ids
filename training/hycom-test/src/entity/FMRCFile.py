cc9ee8e24259f01ad945f206a810ef2ee1a286ed
Darren Adams <dadams@illinois.edu>
2021-09-12 11:25:42 -0500

    debug

--- training/hycom-test/src/entity/FMRCFile.py
+++ training/hycom-test/src/entity/FMRCFile.py
@@ -52,7 +52,8 @@ def download(this):
     try:
         extPath = c3.HycomUtil.downloadToExternal(url, this.id, 'hycom-data')
         updated.status='downloaded'
-        updated.file = c3.File(**{'url': extPath}).readMetadata()
+        #updated.file = c3.File(**{'url': extPath}).readMetadata()
+        updated.file = c3.File(**{'url': extPath})
         updated.merge()
     except Exception as e:
         updated.status = 'error'
