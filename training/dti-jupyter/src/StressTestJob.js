b222cbc837dddb0a04e7867984c2d02871f94d67
Darren Adams <dadams@illinois.edu>
2021-09-16 10:23:12 -0500

    added stress test types

---
+++ training/dti-jupyter/src/StressTestJob.js
@@ -0,0 +1,41 @@
+// FMRCDownloadJob.js
+
+/**
+ * 
+ * @param {StressTestJob} job 
+ * @param {StressTestJobOptions} options 
+ */
+ function doStart(job, options) {
+
+    const chunk = (arr, size) => arr.reduce((acc, e, i) => (i % size ? acc[acc.length - 1].push(e) : acc.push([e]), acc), []);
+
+    // Schedule a batch of jobs
+    function scheduleBatch(batch) {
+        var batchSpec = StressTestBatch.make({values: batch});
+        job.scheduleBatch(batchSpec);
+     }
+
+    let jobs = [];
+    // Make an array of Stress Tests
+    for (var i = 0; i < options.numJobs; i++) {
+        jobs.push(StressTest.make());
+    }
+
+    // Split the array into chunks and schedule each batch
+    batches = chunk(jobs, options.batchSize);
+    batches.forEach(scheduleBatch);
+
+ }
+
+/**
+ * @param {StressTestJobBatch} options  
+ * @param {StressTestJob} job 
+ * @param {StressTestJobOptions} options 
+ * 
+ */
+ function processBatch(batch, job, options) {
+    function runStressTest(stressTest){
+        stressTest.compute_pi(options.computePi_n);
+    } 
+    batch.values.foreach(runStressTest)
+ }
