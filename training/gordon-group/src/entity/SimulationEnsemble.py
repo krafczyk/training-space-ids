a10e0675d393ff34841955e0b2da616c1f207af4
Darren Adams <dadams@illinois.edu>
2021-09-24 16:19:43 -0500

    seeded a single SimulationEnsemble row

---
+++ training/gordon-group/src/entity/SimulationEnsemble.py
@@ -0,0 +1,13 @@
+
+def loadOutputFiles(this):
+    """
+     - Create SimulationOutPutFile records if they don't exist
+     - Load the output files into the SimulationOutputFile records
+    """
+
+    # # Create SimulationOutPutFile records if they don't exist
+    # this.createSimulationOutputFiles()
+
+    # # Load the output files into the SimulationOutPutFile records
+    # this.loadSimulationOutputFiles()
+    pass
