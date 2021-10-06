0efecf47d15b99f01b245ce1869a9084394ef038
Darren Adams <dadams@illinois.edu>
2021-10-06 10:27:37 -0500

    added convenience function for downloading a hycom file locally #360

--- training/hycom-test/src/type/HycomUtil.py
+++ training/hycom-test/src/type/HycomUtil.py
@@ -1,3 +1,16 @@
+def downloadToLocal(srcUrl, fileName, localDir="/tmp"):
+    """
+    """
+    import os
+    import requests
+    tmp_path = localDir + "/" + fileName
+    with requests.get(srcUrl, stream=True) as r:
+        r.raise_for_status()
+        with open(tmp_path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    return tmp_path
+
 def downloadToExternal(srcUrl, fileName, extDir):
     """
     """
