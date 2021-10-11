74cb496336da0250b4d998533690d8b7036dfb14
Darren Adams <dadams@illinois.edu>
2021-10-11 07:41:53 -0500

    more

--- training/hycom-test/src/batch/FMRCDownloadJob.js
+++ training/hycom-test/src/batch/FMRCDownloadJob.js
@@ -9,7 +9,7 @@
     var batch = []
 
     var dataset = FMRCFile.fetchObjStream({
-        include: '[this, fmrc.expired,fmrc.downloadOptions,fmrc.subsetOptions]',
+        include: '[this, fmrc.expired,fmrc.downloadOptions,fmrc.subsetOptions,fmrc.urlPath,fmrc.timeCoverage]',
         filter: "status != 'downloaded' && fmrc.expired=='false'",
         limit: options.limit,
     });
