d5660d1084b8c837bd2c592e3f3abbbf94cb3aac
babreu-ncsa <babreu@illinois.edu>
2021-10-21 10:22:13 -0500

    C3DS suggestion didnt work... dont want to mess this up now

--- training/gordon-group/src/entity/SimulationOutputFile.py
+++ training/gordon-group/src/entity/SimulationOutputFile.py
@@ -11,7 +11,6 @@ def upsertData(this):
     from datetime import datetime, timedelta
     import pandas as pd
 
-    #if(this.processed == False):
     # open file
     sample = c3.NetCDFUtil.openFile(this.file.url)
     
@@ -45,31 +44,26 @@ def upsertData(this):
     versionTag= -1 * diff_time.total_seconds()
     df['versionTag'] = versionTag
 
-    output_records = df.to_dict(orient="records")
-
+#    output_records = df.to_dict(orient="records")
     # create list of SimulationModelOutput objs
-#    parent_id = "SMOS_" + this.simulationSample.id
-#    now_time = datetime.now()
-#    diff_time = (now_time - zero_time)
-#    versionTag= -1 * diff_time.total_seconds()
-#    output_records = [
-#        c3.SimulationModelOutput(**{
-#            'longitude': df['longitude'].iloc[i],
-#            'latitude': df['latitude'].iloc[i],
-#            'mass_BC_acc': df['mass_BC_acc'].iloc[i],
-#            'mass_BC_Ait': df['mass_BC_Ait'].iloc[i],
-#            'mass_BC_Aitins': df['mass_BC_Aitins'].iloc[i],
-#            'mass_BC_cor': df['mass_BC_cor'].iloc[i],
-#            'mass_OC_acc': df['mass_OC_acc'].iloc[i],
-#            'mass_OC_Ait': df['mass_OC_Ait'].iloc[i],
-#            'mass_OC_Aitins': df['mass_OC_Aitins'].iloc[i],
-#            'mass_OC_cor': df['mass_OC_cor'].iloc[i],
-#            'start': df['datetime'].iloc[i],
-#            'parent': parent_id,
-#            'dataVersion': versionTag
-#        })
-#        for i in range(len(df))
-#    ]
+    output_records = [
+        c3.SimulationModelOutput(**{
+            'longitude': df['longitude'].iloc[i],
+            'latitude': df['latitude'].iloc[i],
+            'mass_BC_acc': df['mass_BC_acc'].iloc[i],
+            'mass_BC_Ait': df['mass_BC_Ait'].iloc[i],
+            'mass_BC_Aitins': df['mass_BC_Aitins'].iloc[i],
+            'mass_BC_cor': df['mass_BC_cor'].iloc[i],
+            'mass_OC_acc': df['mass_OC_acc'].iloc[i],
+            'mass_OC_Ait': df['mass_OC_Ait'].iloc[i],
+            'mass_OC_Aitins': df['mass_OC_Aitins'].iloc[i],
+            'mass_OC_cor': df['mass_OC_cor'].iloc[i],
+            'start': df['datetime'].iloc[i],
+            'parent': parent_id,
+            'dataVersion': versionTag
+        })
+        for i in range(len(df))
+    ]
 
     # upsert this batch
     c3.SimulationModelOutput.upsertBatch(output_records)
@@ -77,6 +71,4 @@ def upsertData(this):
     this.processed = True
     c3.SimulationOutputFile.merge(this)
     return True
-    #else:
-    #    return False
     
