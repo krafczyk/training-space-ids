65f92540941712e5eacdfdb8aeaeeb288c3969cf
babreu-ncsa <babreu@illinois.edu>
2022-02-22 11:00:05 -0600

    merging object

--- training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
+++ training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
@@ -6,5 +6,6 @@ def build(this):
     kernel = ConstantKernel(this.constantValue)
 
     this.pickledKernel = c3.PythonSerialization.serialize(obj=kernel)
+    c3.SklearnGPRKernelConstant.merge(this)
 
     return
