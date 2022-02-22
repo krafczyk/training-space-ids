59529f87e8ea0d00ec7926ad41089206cfbbb456
babreu-ncsa <babreu@illinois.edu>
2022-02-22 13:57:49 -0600

    hiding push

--- training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
+++ training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
@@ -3,7 +3,7 @@ def build(this):
     from sklearn.gaussian_process.kernels import ConstantKernel
 
     this.name = 'Constant'
-    this.hyperParameters.push(this.constantValue)
+    #this.hyperParameters.push(this.constantValue)
     kernel = ConstantKernel(this.constantValue)
 
     this.pickledKernel = c3.PythonSerialization.serialize(obj=kernel)
