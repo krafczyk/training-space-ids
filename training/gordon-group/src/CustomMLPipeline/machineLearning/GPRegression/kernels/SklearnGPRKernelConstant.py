8f219ecaf9e8dcc7adb89cd439027ee3d6380af9
babreu-ncsa <babreu@illinois.edu>
2022-02-22 13:15:02 -0600

    i don't want entity types

--- training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
+++ training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
@@ -3,10 +3,9 @@ def build(this):
     from sklearn.gaussian_process.kernels import ConstantKernel
 
     this.name = 'Constant'
+    this.hyperParameters.push(this.ConstantValue)
     kernel = ConstantKernel(this.constantValue)
 
     this.pickledKernel = c3.PythonSerialization.serialize(obj=kernel)
-
-    c3.SklearnGPRKernelConstant.merge(this)
 
     return
