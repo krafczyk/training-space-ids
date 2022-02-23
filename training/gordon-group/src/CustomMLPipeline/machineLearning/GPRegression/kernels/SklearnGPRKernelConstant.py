6282368519f95803f49b89e1d3adb2ed7557583c
babreu-ncsa <babreu@illinois.edu>
2022-02-23 07:31:37 -0600

    creating object inside build function

--- training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
+++ training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
@@ -3,11 +3,23 @@ def build(this):
     from sklearn.gaussian_process.kernels import ConstantKernel
     import numpy as np
 
-    this.kernel.name = 'Constant'
-    hyperPars = [this.constantValue]
-    this.kernel.hyperParameters = hyperPars
+#    this.kernel.name = 'Constant'
+#    hyperPars = [this.constantValue]
+#    this.kernel.hyperParameters = hyperPars
+#    sklKernel = ConstantKernel(this.constantValue)
+#
+#    this.kernel.pickledKernel = c3.PythonSerialization.serialize(obj=sklKernel)
+
     sklKernel = ConstantKernel(this.constantValue)
 
-    this.kernel.pickledKernel = c3.PythonSerialization.serialize(obj=sklKernel)
+    kernel_pickled = c3.PythonSerialization.serialize(obj=sklKernel)
+    kernel_name = 'Constant'
+    kernel_hyperParameters = [this.constantValue]
+
+    this.kernel = c3.SklearnGPRKernel({
+        name=kernel_name,
+        hyperParameters=kernel_hyperParameters,
+        pickledKernel=kernel_pickled
+    })
 
     return this
