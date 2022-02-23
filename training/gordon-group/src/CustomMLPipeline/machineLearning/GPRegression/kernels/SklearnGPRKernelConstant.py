0774caa381a29f41b016746c651f6bc1151800a8
babreu-ncsa <babreu@illinois.edu>
2022-02-23 07:14:12 -0600

    changing type definition a little bit

--- training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
+++ training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/kernels/SklearnGPRKernelConstant.py
@@ -3,11 +3,11 @@ def build(this):
     from sklearn.gaussian_process.kernels import ConstantKernel
     import numpy as np
 
-    this.name = 'Constant'
+    this.kernel.name = 'Constant'
     hyperPars = [this.constantValue]
-    this.hyperParameters = hyperPars
-    kernel = ConstantKernel(this.constantValue)
+    this.kernel.hyperParameters = hyperPars
+    sklKernel = ConstantKernel(this.constantValue)
 
-    this.pickledKernel = c3.PythonSerialization.serialize(obj=kernel)
+    this.kernel.pickledKernel = c3.PythonSerialization.serialize(obj=sklKernel)
 
     return this
