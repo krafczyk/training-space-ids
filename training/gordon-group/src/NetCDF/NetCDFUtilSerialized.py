ac7459f313f0a50e139d71f6850023bbee1d4ad3
babreu-ncsa <babreu@illinois.edu>
2022-02-28 10:19:12 -0600

    add serialized netcdf util

---
+++ training/gordon-group/src/NetCDF/NetCDFUtilSerialized.py
@@ -0,0 +1,38 @@
+def openFile(url, localPath='/tmp'):
+    """
+    Opens a NetCDF file from an external storage path (e.g. Azure blob)
+
+    Arguments:
+        -url (str): URL to NetCDF file
+        -localPath (str): Path to the local file
+    Returns:
+        -netCDF4.Dataset: A netCDF4 Dataset object
+    """
+    import netCDF4 as nc
+    import os
+    filename = os.path.basename(url)
+    tmp_path = localPath + '/' + filename
+    c3.Client.copyFilesToLocalClient(url, '/tmp')
+    pickledNC = c3.PythonSerialization.serialize(obj=nc.Dataset(tmp_path))
+
+    return pickledNC
+
+
+def closeFile(ds, url, localPath='/tmp'):
+    """
+    Closes a NetCDF file.
+
+    Arguments:
+        -ds (netCDF4.Dataset): A netCDF4 Dataset object
+        -url (str): URL to a NetCDF file
+        -localPath (str): Path to the local file
+    Returns:
+        -integer
+    """
+    import os
+    ds.close()
+    filename = os.path.basename(url)
+    tmp_path = localPath + '/' + filename
+    os.remove(tmp_path)
+
+    return 1
