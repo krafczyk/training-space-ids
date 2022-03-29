38acd4330e8263dde98a6b3d471bfead1e327a3a
deuce1957 <xavierw@kth.se>
2022-03-29 15:25:21 +0200

    Add: Requirements, Rem: Extraneous Packages

--- training/hycom-test/script/download.py
+++
@@ -1,9 +0,0 @@
-def downloadToS3External(srcUrl, fileName, s3_folder):
-    r = requests.get(srcUrl)
-    tmp_path = "/tmp/" + fileName
-    with open(tmp_path, 'wb') as f:
-        f.write(r.content)
-    c3.Client.uploadLocalClientFiles(tmp_path, s3_folder, {"peekForMetadata": True})
-    logger.info("file {} downloaded to {}".format(fileName, s3_folder + fileName))
-    os.remove(tmp_path)
-    return s3_folder + fileName
--- training/hycom-test/script/stream.py
+++
@@ -1,8 +0,0 @@
-# From https://community.c3.ai/t/how-to-stream-csv-files/4321/3
-def streamFromS3(file, modelo = "Modelo_2020_02_27", geografia = "cat",**kwargs):
-    logger.info("About to stream file "+ file)
-    myfile=c3.S3File(url=c3.S3FileSystem().mounts()['DEFAULT'] +modelo+"/"+geografia+"/"+file)
-    csv_string=StringIO(myfile.readString())
-    mydf= pd.read_csv(csv_string,**kwargs)
-    logger.info("File "+file+" streamed")
-    return mydf
