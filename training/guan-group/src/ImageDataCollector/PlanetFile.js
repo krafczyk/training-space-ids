89af7d46ae211a2ade9ec3831373e22b6f7380a4
Yifang Zhang <zhang303@illinois.edu>
2022-04-20 16:05:51 -0500

    remove the beforeRemove function

--- training/guan-group/src/ImageDataCollector/PlanetFile.js
+++ training/guan-group/src/ImageDataCollector/PlanetFile.js
@@ -1,4 +1,13 @@
 
 function beforeRemove(objs) {
-   return -1;
+    var typeName = objs[0].toJson()['type']
+    objs.forEach(function(f) {
+       if (f.raw_image_file.exists()){
+          f.raw_image_file.delete();
+       }
+       if (f.processed_image_file.exists()){
+          f.processed_image_file.delete();
+       }
+    });
+    return c3Make("ObjList<"+typeName+">",{objs: objs});
 };
