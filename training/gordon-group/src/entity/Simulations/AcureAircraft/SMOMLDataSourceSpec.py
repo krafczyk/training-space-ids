a08d7496fd612ba1176f5ed4f0c9f503e06831ea
babreu-ncsa <babreu@illinois.edu>
2022-05-10 14:52:08 -0500

    commenting out target

--- training/gordon-group/src/entity/Simulations/AcureAircraft/SMOMLDataSourceSpec.py
+++ training/gordon-group/src/entity/Simulations/AcureAircraft/SMOMLDataSourceSpec.py
@@ -16,31 +16,32 @@ def getInputDataForSources(this, srcIds):
     X = df[df.columns[5:]]
 
     # now grab the targets
-    metricName = "Average_" + this.targetName + "_SimulationSample"
-    metricDescr = "Calculates average of " + this.targetName + " for a given set of SimulationSample"
-    metricExpr = "avg(avg(normalized.data." + this.targetName + "))"
-    metric = c3.SimpleMetric(
-        id = metricName,
-        name = metricName,
-        description = metricDescr,
-        srcType = "SimulationSample",
-        path = "output",
-        expression = metricExpr
-    )
-    spec = c3.EvalMetricsSpec(
-        ids = simulationSamples['id'],
-        expressions = [metricName],
-        start = this.timestamp,
-        end = this.timestamp,
-        interval = 'SECOND'
-    )
-    evalMetricsResult = c3.SimulationSample.evalMetricsWithMetadata(
-        spec = spec,
-        overrideMetrics = [metric]
-    )
-    y = c3.EvalMetricsResult.toPandas(result=evalMetricsResult)
-    
+   # metricName = "Average_" + this.targetName + "_SimulationSample"
+   # metricDescr = "Calculates average of " + this.targetName + " for a given set of SimulationSample"
+   # metricExpr = "avg(avg(normalized.data." + this.targetName + "))"
+   # metric = c3.SimpleMetric(
+   #     id = metricName,
+   #     name = metricName,
+   #     description = metricDescr,
+   #     srcType = "SimulationSample",
+   #     path = "output",
+   #     expression = metricExpr
+   # )
+   # spec = c3.EvalMetricsSpec(
+   #     ids = simulationSamples['id'],
+   #     expressions = [metricName],
+   #     start = this.timestamp,
+   #     end = this.timestamp,
+   #     interval = 'SECOND'
+   # )
+   # evalMetricsResult = c3.SimulationSample.evalMetricsWithMetadata(
+   #     spec = spec,
+   #     overrideMetrics = [metric]
+   # )
+   # y = c3.EvalMetricsResult.toPandas(result=evalMetricsResult)
+   # 
     X = c3.Dataset.fromPython(X)
-    y = c3.Dataset.fromPython(y)
+    #y = c3.Dataset.fromPython(y)
 
-    return [X,y]
+    return X
+   # return [X,y]
