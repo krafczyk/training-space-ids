f319a58f616060a47eb1b26abc3a1da5058580f5
babreu-ncsa <babreu@illinois.edu>
2022-05-16 15:21:24 -0500

    fetch input data

--- training/gordon-group/src/entity/Simulations/AcureAircraft/SMOMLDataSourceSpec.py
+++ training/gordon-group/src/entity/Simulations/AcureAircraft/SMOMLDataSourceSpec.py
@@ -10,38 +10,13 @@ def getInputDataForSources(this, srcIds):
     #from sklearn.model_selection import train_test_split
 
     # fetch features
-    features = c3.SimulationModelParameters.fetch({"limit": -1}).objs.toJson()
+    features = c3.SimulationModelParameters.fetch({"limit": -1, "order": "id"}).objs.toJson()
     df = pd.DataFrame(features)
-    #simulationSamples = pd.DataFrame(df['id'])
     X = df[df.columns[5:]]
+    X = df.sample(frac=this.leaveBehindRatio, random_state=this.randomSeed)
 
-    # now grab the targets
-   # metricName = "Average_" + this.targetName + "_SimulationSample"
-   # metricDescr = "Calculates average of " + this.targetName + " for a given set of SimulationSample"
-   # metricExpr = "avg(avg(normalized.data." + this.targetName + "))"
-   # metric = c3.SimpleMetric(
-   #     id = metricName,
-   #     name = metricName,
-   #     description = metricDescr,
-   #     srcType = "SimulationSample",
-   #     path = "output",
-   #     expression = metricExpr
-   # )
-   # spec = c3.EvalMetricsSpec(
-   #     ids = simulationSamples['id'],
-   #     expressions = [metricName],
-   #     start = this.timestamp,
-   #     end = this.timestamp,
-   #     interval = 'SECOND'
-   # )
-   # evalMetricsResult = c3.SimulationSample.evalMetricsWithMetadata(
-   #     spec = spec,
-   #     overrideMetrics = [metric]
-   # )
-   # y = c3.EvalMetricsResult.toPandas(result=evalMetricsResult)
-   # 
+    # cast it into 
     X = c3.Dataset.fromPython(X)
-    #y = c3.Dataset.fromPython(y)
 
     return 0
    # return [X,y]
