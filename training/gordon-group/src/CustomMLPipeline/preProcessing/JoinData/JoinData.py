df478a64176489eec95b0af965826c6328910d40
James Carzon <jcarzon@andrew.cmu.edu>
2022-06-22 12:12:56 -0400

    Fetch and join data in separate functions
    Firstly, methods for a c3 type must respect the method names already
    known to the inherited class. We change the names for clarity. Secondly,
    for simplicity, we divide the task of JoinData between two methods.
    
    This commit is still probably not quite what the type is supposed to
    look like for the purpose of an MLTrainingJob, but I haven't seen a
    specific example of how exactly an MLTrainingJob ranges over a list of
    latitudes or longitudes or times and instantiates an MLDataSourceSpec
    for each combination of values. So this is just another trial for
    experimentation purposes.

--- training/gordon-group/src/CustomMLPipeline/preProcessing/JoinData/JoinData.py
+++ training/gordon-group/src/CustomMLPipeline/preProcessing/JoinData/JoinData.py
@@ -1,30 +1,44 @@
-def joinData(this, input):
+def getInputDataForSources(this, srcIds):
     """
-    input : geoSurfaceTimePoint
+    srcIds : <?>
     """
     import pandas as pd
 
     # Collect the inputs
     inputTablePandas = pd.DataFrame(c3.SimulationModelParameters.fetch().objs.toJson())
 
+    # Create ID column for simulation run
+    inputTablePandas["simID"] = [int(inputTablePandas["id"][k].split("_")[-1]) for k in range(len(inputTablePandas.id))] 
+
+    return c3.Dataset.fromPython(inputTablePandas.select_dtypes(["number"]))
+
+
+def combineInputDataObjects(this, dataObjs):
+    """
+    dataObjs : geoSurfaceTimePoint
+    """
+
+    # Get input table
+    inputTablePandas = c3.Dataset.toPandas(this.getInputDataForSources())
+
     # Collect the outputs from the geoSpaceTimePoint
-    s3haodFilter = c3.Filter().eq("geoSurfaceTimePoint", input)
+    s3haodFilter = c3.Filter().eq("geoSurfaceTimePoint", dataObjs)
     outputTableC3 = c3.Simulation3HourlyAODOutput.fetch(
         {"filter": s3haodFilter, "limit": -1}
     )
     outputTablePandas = pd.DataFrame(outputTableC3.objs.toJson())
-    outputTablePandas["id"] = [row["id"] for row in outputTablePandas.simulationSample]
+    outputTablePandas["simID"] = [int(row["id"]) for row in outputTablePandas.simulationSample]
 
     # Join the outputs with the input table
     joinedData = pd.merge(
-        inputTablePandas, # Input table -> DataFrame
+        inputTablePandas,
         outputTablePandas,
-        on="id",
+        on="simID",
         how="inner"
     )
 
     # Add identifier from the geoSurfaceTimePoint to each row
     nrows = joinedData.shape[0]
-    joinedData["point"] = [input[k]["id"] for k in range(nrows)]
+    joinedData["point"] = [dataObjs[k]["id"] for k in range(nrows)]
 
-    return this
+    return joinedData
