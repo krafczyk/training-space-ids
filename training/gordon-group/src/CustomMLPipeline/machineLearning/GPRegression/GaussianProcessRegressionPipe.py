032d28bc63202c66eb636e8957b9f456a3e6a214
babreu-ncsa <babreu@illinois.edu>
2022-06-29 10:09:21 -0500

    change in train

--- training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/GaussianProcessRegressionPipe.py
+++ training/gordon-group/src/CustomMLPipeline/machineLearning/GPRegression/GaussianProcessRegressionPipe.py
@@ -7,7 +7,7 @@ def train(this, input, targetOutput, spec):
 
     # get data
     X = c3.Dataset.toNumpy(dataset=input)
-    y = c3.Dataset.toNumpy(dataset=targetOutput).flatten()
+    y = c3.Dataset.toNumpy(dataset=targetOutput)#.flatten()
 
     # get kernel object from c3, make it python again
     kernel = c3.PythonSerialization.deserialize(serialized=this.technique.kernel.pickledKernel)
@@ -16,30 +16,6 @@ def train(this, input, targetOutput, spec):
     gp = GaussianProcessRegressor(kernel=kernel)
     gp.fit(X, y)
 
-    # this step for updating the kernel parameters is written specifically for the Matern parameters
-    # Bruno - we may want to do this outside this method
-    # since this one is supposed to work regardless the kernel choice
-    #if this.kernel.kernel_name is 'Matern':
-#
-    #    # record kernel parameters now that they are fitted
-    #    fit_params = gp.kernel_.get_params()
-#
-    #    updated_hyperParameters = c3.c3Make(
-    #        "map<string, double>", {
-    #            "lengthScale": list(fit_params['k2__length_scale']),
-    #            "coefficient": fit_params['k1'],
-    #            "nu": fit_params['k2__nu']
-    #        }
-    #    )
-#
-    #    # restate the kernel using the fitted parameters
-    #    this.kernel = c3.SklearnGPRKernel(
-    #        name=this.kernel.kernel_name,
-    #        hyperParameters=updated_hyperParameters,
-    #        pickledKernel=this.kernel.kernel_pickled
-    #    )
-
-    # pickle model
     this.trainedModel = c3.MLTrainedModelArtifact(model=c3.PythonSerialization.serialize(obj=gp))
 
     return this
