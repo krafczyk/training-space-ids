37571fe7221e40711a3f40e1da0d5a3006a2bce5
babreu-ncsa <babreu@illinois.edu>
2022-07-15 15:05:07 -0500

    forget about reduce for now

--- training/gordon-group/src/MapReduceJobs/CollectGPRLearnedParameters.py
+++ training/gordon-group/src/MapReduceJobs/CollectGPRLearnedParameters.py
@@ -1,12 +1,12 @@
 def reduce(outKey, interValues, job):
-    values = []
-
-    for iv in interValues:
-        for val in iv:
-            pickledModel = val["trainedModel"]["model"]
-            model = c3.PythonSerialization.deserialize(serialized=pickledModel)
-            hp = model.kernel_.get_params()['k2__length_scale']
-            model_id = val["id"]
-            values.append((hp, model_id))
-
-    return values
+    #values = []
+#
+    #for iv in interValues:
+    #    for val in iv:
+    #        pickledModel = val["trainedModel"]["model"]
+    #        model = c3.PythonSerialization.deserialize(serialized=pickledModel)
+    #        hp = model.kernel_.get_params()['k2__length_scale']
+    #        model_id = val["id"]
+    #        values.append((hp, model_id))
+#
+    return 1
