913e56bc916baaadcae174a7c9bd687318b3159c
babreu-ncsa <babreu@illinois.edu>
2022-07-15 14:58:01 -0500

    syntax fix

--- training/gordon-group/src/MapReduceJobs/CollectGPRLearnedParameters.js
+++ training/gordon-group/src/MapReduceJobs/CollectGPRLearnedParameters.js
@@ -7,7 +7,7 @@
 function map(batch, objs, job) {
     var dict = {};
     var models = objs.forEach(function (obj) {
-        AODModelFinder.getPipe(
+        AODGPRModelFinder.getPipe(
             job.context.excludeFeatures, 
             obj.id,
             job.context.targetName,
@@ -15,28 +15,4 @@ function map(batch, objs, job) {
     });
     dict[batch] = models;
     return dict;
-}
-
-/**
- * @param {string} outKey Output key this function is being called for
- * @param {int[]} interValues Intermediate counts for the same word
- * @param {MapReduce} job The job instance
- * @returns {int[]} A single value array with the number of times a word occurred
- */
-function reduce(outKey, interValues, job) {
-    var values = [];
-
-    for (var i = 0; i < interValues.length; i++) {
-        var iv = interValues[i];
-        for (var j = 0; j < iv.length; j++) {
-            var val = iv[j];
-            var pickledModel = val["trainedModel"]["model"];
-            var model = PythonSerialization.deserialize(serialized=pickledModel);
-            var hp = model.kernel_.get_params()['k2__length_scale'];
-            var model_id = val["id"];
-            values.append([hp, model_id]);
-        };
-    };
-
-    return values;
 }
