6c6c4ed302e23ea6a47f02ccc91f7b22559c0f9e
Bruno Abreu <babreu@illinois.edu>
2022-10-04 14:38:42 -0500

    cleaner bur likely slower

--- training/gordon-group/src/Utils/DataStaging/StagedFeatures.py
+++ training/gordon-group/src/Utils/DataStaging/StagedFeatures.py
@@ -13,54 +13,21 @@ def stageFromAODGPRModelIdsList(ids):
 
     c3.StagedFeatures.removeAll()
 
-    # get data from dataSourceSpec one model
-    model = c3.GaussianProcessRegressionPipe.get(ids[0], "dataSourceSpec")
-    data_source_spec = c3.GPRDataSourceSpec.get(model.dataSourceSpec.id)
-    excludeFeatures = data_source_spec.excludeFeatures
-    featuresType = data_source_spec.featuresType.toType()
-    inputTableC3 = featuresType.fetch(data_source_spec.featuresSpec).objs.toJson()
-    inputTable = pd.DataFrame(inputTableC3)
-    inputTable = inputTable.drop("version", axis=1)
-    inputTable = inputTable.select_dtypes(["number"])
-    if (excludeFeatures):
-        inputTable.drop(columns=excludeFeatures, inplace=True)
-
-    # get gstp coordinates from each model
-    lats = []
-    lons = []
-    times = []
+    df = pd.DataFrame()
     for model_id in ids:
-        model = c3.GaussianProcessRegressionPipe.get(model_id, "dataSourceSpec")
+        model = c3.GaussianProcessRegressionPipe.get(model_id)
         data_source_spec = c3.GPRDataSourceSpec.get(model.dataSourceSpec.id, "targetSpec")
         gstp_id = data_source_spec.targetSpec.filter.split(" == ")[1].replace('"', '')
         gstp = c3.GeoSurfaceTimePoint.get(gstp_id)
-        lats.append(gstp.latitude)
-        lons.append(gstp.longitude)
-        times.append(gstp.time)
+        pdf = c3.Dataset.toPandas(model.getFeatures())
+        pdf["latitude"] = gstp.latitude
+        pdf["longitude"] = gstp.longitude
+        df = pd.concat([df,pdf], ignore_index=True)
 
-    def row_to_dict(row):
-        d = {}
-        for col in row.index:
-            d[col] = row[col]
-        return d
-
-    def add_coords(obj, lat, lon):
-        obj["latitude"] = lat
-        obj["longitude"] = lon
-        return
-    
-    # build dataframe
-    df_sim_par = pd.DataFrame()
-    df_sim_par["features"] = inputTable.apply(row_to_dict, axis=1)
-
-    df = pd.DataFrame()
-    for i in range(len(lats)):
-        df_to_add = df_sim_par.copy()
-        df_to_add["features"].apply(add_coords, args=(lats[i], lons[i]))
-        df = pd.concat([df,df_to_add], ignore_index=True)
-
-    df["id"] = df.index
-    output_records = df.to_dict(orient="records")
+    df_final = pd.DataFrame()
+    df_final["features"] = df.apply(row_to_dict, axis=1)
+    df_final["id"] = df_final.index
+    output_records = df_final.to_dict(orient="records")
     c3.StagedFeatures.upsertBatch(objs=output_records)
 
     return 0
